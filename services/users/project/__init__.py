f97dcfb766bd9033a4d79ed92fec2ae9f00543c9
Santosh Kc <santosh@artup.io>
2018-04-05 12:59:36 +0545

    migration db

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -7,11 +7,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 
 # instantiate the db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -29,6 +31,7 @@ def create_app(script_info=None):
     # setup extensions
     db.init_app(app)
     toolbar.init_app(app)
+    migrate.init_app(app, db)
 
     # register thje blueprints
     from project.api.users import users_blueprint
